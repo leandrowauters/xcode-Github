4a2991f447ab0055453204c221d2034e8dd9e275
John Doe <leandro.wauters@gmail.com>
2018-10-05 14:54:45 -0400

    initial commit of my xcode project

---
+++ Xcode Github/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Xcode Github
+//
+//  Created by Leandro Wauters on 10/5/18.
+//  Copyright © 2018 Leandro Wauters. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
