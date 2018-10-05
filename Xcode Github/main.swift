7072bb02cafdeaa5c8ad8d370f4742f1afed88ac
John Doe <leandro.wauters@gmail.com>
2018-10-05 15:09:15 -0400

    added todos for code practice

--- Xcode Github/main.swift
+++ Xcode Github/main.swift
@@ -10,3 +10,18 @@ import Foundation
 
 print("Hello, World!")
 
+// control flow
+
+let day= "Friday"
+
+if day == "Friday" {
+    print("The weekend is here")
+} else {
+    print("pursuit happiness")
+}
+
+// TO DO: practice writing switch statements
+
+// TO DO: practice writing switch statements
+
+
